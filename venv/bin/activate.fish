ae4124b8f21ddf2a8c7e5b7f0538f3e467acd71a
George <mukoshig@gmail.com>
2025-11-28 00:09:26 +0300

    Deleted and modified virtual enviroment a system feature

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/praxcedeskabeya/development/code/se-prep/phase4/Backend-Fit-track-app/venv
+set -gx VIRTUAL_ENV /home/gmukoshi/Development/code/phase-4/Backend-Fit-track-app/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
