8599f8f232c27165d3936c658e0313f468863887
George <mukoshig@gmail.com>
2025-11-29 21:50:45 +0300

    added comments on app.py

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_cors import CORS
-
 from .config import Config
 from .models import db
 from .validators import (
